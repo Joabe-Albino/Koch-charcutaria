c34005833d46ad3d1a08ed5bf9d058bfdc9f3fe9
Joabe-Albino <joabefelipealbino@gmail.com>
2025-10-22 20:29:12 -0300

    Adicionado o JS

---
+++ site-kochcharcutaria/view/js/produtos.js
@@ -0,0 +1,34 @@
+//  <div class="container-produtos">
+//         <section>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+//             <div class="produtos"><img src="../src/produtp.png" alt=""></div>
+            
+//         </section>
+//     </div>
+
+
+const body = document.body;
+
+const img = document.createElement("img");
+img.src = "../src/produtp.png";
+
+
+const divProdutos = document.createElement("div");
+divProdutos.classList.add("produtos");
+divProdutos.append(img);
+
+const sectionProdutos = document.createElement("section");
+sectionProdutos.append(divProdutos);
+
+const divContainerProdutos = document.createElement("div");
+divContainerProdutos.classList.add("container-produtos");
+divContainerProdutos.append(sectionProdutos);   
+
+
+body.append(divContainerProdutos);
