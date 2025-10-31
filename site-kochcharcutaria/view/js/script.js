46bd544e27dae50bf61b05e887d61a8c762818ea
Joabe-Albino <joabefelipealbino@gmail.com>
2025-10-31 19:33:02 -0300

    Slider na página de home

---
+++ site-kochcharcutaria/view/js/script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+let index = 0; 
+function moverSlide() {
+    index++;
+
+   
+    if (index >= totalSlides) {
+        index = 0;
+    }
+
+    slides.style.transform = `translateX(-${index * 400}%)`;
+}
+
+
+setInterval(moverSlide, 4000);
