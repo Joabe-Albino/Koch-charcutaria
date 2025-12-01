006356f1c2f2669086b5e1941cc5230f77096625
Joabe-Albino <joabefelipealbino@gmail.com>
2025-12-01 14:54:30 -0300

    Consertando admin para hospedar

--- admin-kochcharcutaria/schemaTypes/index.js
+++
@@ -1,9 +0,0 @@
-import {carrosselType} from './carrosselType'
-import {destaquesHomeType} from './destaquesHomeType'
-import {infoType} from './infoType'
-import {parceirosHomeType} from './parceirosHomeType'
-import {produtosViewType} from './produtosViewType'
-import {revendaType} from './revendaType'
-
-export const schemaTypes = [carrosselType, destaquesHomeType, infoType, parceirosHomeType,
-  produtosViewType, revendaType]
